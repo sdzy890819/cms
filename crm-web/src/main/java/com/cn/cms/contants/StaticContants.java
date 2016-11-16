2d59a79e95dfcf2fb3cfb3bc2eaeece3f4fe4e5a
张 阳 <zhangyang@pro-old.local>
2016-11-16 11:23:34 +0800

    添加基础类

---
+++ crm-web/src/main/java/com/cn/cms/contants/StaticContants.java
@@ -0,0 +1,7 @@
+package com.cn.cms.contants;
+
+/**
+ * Created by zhangyang on 16/11/15.
+ */
+public class StaticContants {
+}
--- crm-web/src/main/java/com/cn/cms/po/Base.java
+++ crm-web/src/main/java/com/cn/cms/po/Base.java
@@ -7,17 +7,30 @@ import lombok.Setter;
 import java.util.Date;
 
 /**
+ * 基础Base类。所有的PO都要继承这个类。
  * Created by zhangyang on 16/11/15.
  */
 @Getter
 @Setter
 public class Base {
 
+    /**
+     * 主键ID
+     */
     protected Long id ;
 
+    /**
+     * 创建时间 系统会自动默认当前时间为创建时间
+     */
     protected Date createTime;
 
+    /**
+     * 修改时间－每次修改都会改变。Table内自动变更
+     */
     protected Date updateTime;
 
+    /**
+     * 删除标记。默认删除状态为 正常状态。
+     */
     protected Integer delTag = DelTagEnum.NORMAL.getType();
 }
