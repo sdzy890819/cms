c1eeed97f96cf4ab7b28a0f779cd0380f4bdcc23
bo.peng <spion@qq.com>
2016-11-17 00:18:26 +0800

    html

---
+++ html/js/data.js
@@ -0,0 +1,18 @@
+ngbody.directive("contentBar", function() {
+	    return {
+	    	restrict : 'E',
+	    	replace : true,
+	    	transclude : true,
+	        templateUrl : '../template/data.html',
+	        controller:function(){
+	        	
+	        },
+	        scope : function(){
+	        	
+	        },
+	        link : function(scope,element){
+
+	        }
+	    }
+   }
+);
