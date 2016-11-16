6359b2da00e4ad4a728c8971ae0b0ea1e055aeec
bo.peng <spion@qq.com>
2016-11-16 22:35:05 +0800

    html

--- html/js/main.js
+++ html/js/main.js
@@ -41,15 +41,20 @@
 	        	var time = 0 , 
 	        		ng = angular.element,
 	        		submenu = element.find('.option li>a') ;
-	        	window.onresize = function(){
+
+	        	function getHeight(){
 	        		clearTimeout(time);
 	        		var height = angular.element(window).height()-angular.element('#Header').height();
 	        		time = setTimeout(function(){
 	        			if(element.height()<height){
 	        				element.height(height)
+	        			}else{
+	        				element.height(element.height())
 	        			}
 	        		}, 300);
 	        	}
+	        	window.onresize = getHeight;
+	        	getHeight();
 	        	function clearMenu(){
 	        		submenu.parent().removeClass('open')
 	        			.find('.arrow').removeClass('cur');
@@ -57,9 +62,29 @@
 	        	submenu.each(function( i , ele ){
 	        		ng(ele).click(function(){
 	        			clearMenu();
-		        		ng(this).parent().addClass('open')
-		        			.find('.arrow').addClass('cur');
+	        			if(this.parentclas){
+	        				this.parentclas = false;
+	        				ng(this).parent().removeClass('open')
+			        			.find('.arrow').removeClass('cur');
+	        			}else{
+	        				this.parentclas=true;
+			        		ng(this).parent().addClass('open')
+			        			.find('.arrow').addClass('cur');
+	        			}
+
+
 		        	});
+	        	});
+
+	        	var tabNav = element.find('.webapp-tab li') , 
+	        		option = element.find('.option');
+	        	tabNav.each(function( i , ele ){
+	        		ng(ele).click(function(){
+	        			tabNav.removeClass('current')
+	        				.eq(i).addClass('current')
+	        			option.hide();
+	        			option.eq(i).show();
+	        		})
 	        	})
 	        }
 	    };
