22f76def8ab84615193ce5b5fb5fdc13c2897910
张阳 <zhangyang04@renrendai.com>
2017-01-09 19:48:46 +0800

    修改publishApplication 自动发布

---
+++ documents/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+cd /data/source/cms
+echo "----------------------------------------------------"
+echo "-----------------------git pull------------------------"
+echo "----------------------------------------------------"
+git pull
+echo "----------------------------------------------------"
+echo "-----------------------mvn package------------------------"
+echo "----------------------------------------------------"
+mvn clean package -e 
+echo "----------------------------------------------------"
+echo "-----------------------operation copy------------------------"
+echo "----------------------------------------------------"
+cd /data/projects/cms-web/
+jar -xvf /data/source/cms/cms-web/target/cms-web.war 
+cp -rf /data/source/cms/cms-publish/target/cms-publish.jar /data/projects/cms-publish/
+echo "----------------------------------------------------"
+echo "-----------------------project restart------------------------"
+echo "----------------------------------------------------"
+/usr/local/tomcat/bin/catalina.sh stop 
+sleep 2s
+ABC=`ps -af |grep "tomcat" |grep "java" |awk '{print $2}'`
+if [[ $ABC > 0 ]]
+then
+	kill -9 $ABC
+fi
+/usr/local/tomcat/bin/catalina.sh start
+
+echo "----------------------------------------------------"
+echo "-----------------------END------------------------"
+echo "----------------------------------------------------"
