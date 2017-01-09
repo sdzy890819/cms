e2775406eb3219b8b71c6251f9fd2c1b8ca0b4c3
张阳 <zhangyang04@renrendai.com>
2017-01-09 22:23:01 +0800

    webapi

--- cms-web/src/main/java/com/cn/cms/web/controller/BaseController.java
+++ cms-web/src/main/java/com/cn/cms/web/controller/BaseController.java
@@ -4,12 +4,14 @@ import com.cn.cms.contants.StaticContants;
 import com.cn.cms.logfactory.CommonLog;
 import com.cn.cms.logfactory.CommonLogFactory;
 import com.cn.cms.utils.CookieUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by zhangyang on 16/11/24.
  */
+@RequestMapping(value="/webapi/")
 public class BaseController {
 
     public CommonLog log = CommonLogFactory.getLog(this.getClass());
