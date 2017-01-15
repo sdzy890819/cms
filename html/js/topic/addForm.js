246204e364bf9436eac6957b5cdc62a5b1234700
bo.peng <spion@qq.com>
2017-01-15 20:10:46 +0800

    me

--- html/js/topic/addForm.js
+++ html/js/topic/addForm.js
@@ -88,37 +88,68 @@ define(['../data/getData','../moduls/Tool'],function(getData,Tool){
 			type : 'text'
 		}
 	];
-	function setData(obj,name,_data){
-		if(obj.type=='select'){
-			if(obj.select[0][0].title==name){
-				obj.select[0] = obj.select[0].concat(Tool.changeObjectName(_data.data,[{name:'columnName',newName:'name'}]));
+	function setData(obj){
+		var self = obj.self;
+		if(self.type=='select'){
+			if(self.select[0][0].title==obj.title){
+				self.select[0] = self.select[0].concat(Tool.changeObjectName(obj.data.data,[{name:obj.changeName,newName:'name'}]));
 			}
 		}
 	}
 	function getList(callback){
 		getData.topic.topicColumnList({//系列专题是。topicColumn
 			callback:function(_data){
-				$.each(list,function(i , obj){
-					if($.type(obj)=='array'){
-						$.each(obj,function(){
-							setData(this,'topicColumnId',_data);
-						})
-					}else{
-						setData(obj,'topicColumnId',_data);
-					}
-				});
 				getData.topic.topicClassifyList({//专题分类 topicClassifyId
 					callback : function( _data1 ){
-						$.each(list,function(i , obj){
-							if($.type(obj)=='array'){
-								$.each(obj,function(){
-									setData(this,'topicClassifyId',_data1);
+						getData.category.listCategory({//获取部门分类列表 categoryId
+							callback : function( _data2 ){
+								$.each(list,function(i , obj){
+									if($.type(obj)=='array'){
+										$.each(obj,function(){
+											setData({
+												self : this,
+												title : 'topicColumnId',
+												data : _data ,
+												changeName : 'columnName'
+											});
+											setData({
+												self : this,
+												title : 'topicClassifyId',
+												data : _data1 ,
+												changeName : 'classifyName'
+											});
+											setData({
+												self : this,
+												title : 'categoryId',
+												data : _data2 ,
+												changeName : 'categoryName'
+											});
+										})
+									}else{
+										setData({
+											self : this,
+											title : 'topicColumnId',
+											data : _data ,
+											changeName : 'classifyName'
+										});
+										setData({
+											self : this,
+											title : 'topicClassifyId',
+											data : _data1 ,
+											changeName : 'classifyName'
+										});
+										setData({
+											self : this,
+											title : 'categoryId',
+											data : _data2 ,
+											changeName : 'categoryName'
+										});
+									}
+
 								})
-							}else{
-								setData(obj,'topicClassifyId',_data1);
+								callback(list);
 							}
-						})
-						callback(list);
+						});
 					}
 				});
 
