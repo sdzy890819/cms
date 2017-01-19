ebb972276e7a307be40eee391be3d2eecb5ca459
bo.peng <spion@qq.com>
2017-01-20 00:31:13 +0800

    me

--- html/js/upload/uploadify/index.js
+++ html/js/upload/uploadify/index.js
@@ -11,14 +11,7 @@ define(["app",'jquery','../../data/URL'],function (app,$,URL) {
 				templateUrl: '../template/upload/videoPop.html',
 				size: 'lg',
 				controller: function($scope,$uibModalInstance,$css,Upload , $timeout) {
-					angular.extend($scope,{
-						uploadPic : function(file) {							
-							
-					    },
-					    close : function(){
-					    	$uibModalInstance.dismiss('cancel');
-					    }
-					});
+					
 				}
 			});
 			pop.opened.then(function (selectedItem) {
@@ -28,7 +21,18 @@ define(["app",'jquery','../../data/URL'],function (app,$,URL) {
 				        "fileObjName" : 'file',
 				        "buttonText" : '请选择视频文件',
 				        'buttonClass' : 'layui-upload-icon',
-				        'uploader' : URL.video.uploadVideo2
+				        'uploader' : URL.video.uploadVideo2,
+				        onError : function(){
+				        	alert('视频上传错误，不支持破损视频')
+				        },
+				        onProgress   : function(file, e) {
+				        	debugger;
+				            if (e.lengthComputable) {
+				                var percent = Math.round((e.loaded / e.total) * 100);
+				            }
+				            file.queueItem.find('.fileinfo').html(' - ' + percent + '%');
+				            file.queueItem.find('.progress-bar').css('width', percent + '%');
+				        }
 				        // Put your options here
 				    });
 				},300);
