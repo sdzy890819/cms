4b92bc71ef3e1c960b8f9b90a3805ff8210c6828
spion@qq.com <spion@qq.com>
2017-01-20 11:57:39 +0800

    me

---
+++ html/js/config.js
@@ -0,0 +1,58 @@
+require.config({
+    baseUrl: 'js/',
+    paths: {
+        'jquery': 'plug/jquery-1.11.2.min',
+        //'jquery-migrate': 'plug/jquery-migrate-3.0.0.min',
+        'wangEditor' : '../wangEditor/dist/js/wangEditor.min',
+
+
+        'angular': 'plug/angular',
+        "angularAMD": "plug/angularAMD.min",
+        "ngload": "plug/ngload",
+        'angular-ui-router': 'plug/angular-ui-router.min',
+        'angular-css':'plug/angular-css.min',
+        'ui-bootstrap':'plug/ui-bootstrap-tpls-2.3.2.min',
+        'angularFileUpload':'plug/angular-file-upload.min',
+
+        //upload img
+        'ngFileUpload-shim' : 'plug/ng-file-upload/dist/ng-file-upload-shim.min',
+        'ngFileUpload' : 'plug/ng-file-upload/dist/ng-file-upload.min',
+
+        //upload video
+        'uploadify' : 'upload/uploadify/jquery.uploadify.min',
+
+        'layui' : 'plug/layui/layui',
+        'jcrop' : 'plug/jcrop/jquery.Jcrop.min',
+
+        'head' : 'common/header' , 
+        'menu' : 'common/menu' , 
+        'form' : 'common/form',        
+        'formlist' : 'common/formlist',
+        'position' : 'common/position',
+        'fixedNav' : 'common/positionNav',
+    },
+    shim: {
+        'jquery': {exports: '$'},
+        //'jquery-migrate': ['jquery'],
+        'jcrop': ['jquery'],
+        "uploadify": ["jquery"],
+
+
+        'angular': {exports: 'angular'},
+        'angular-ui-router': {
+            deps: ['angular'] , 
+            exports: 'angular-ui-router'
+        },
+        'angular-css': {
+            deps: ['angular'] , 
+            exports: 'angular-css'
+        },
+        'ui-bootstrap': ["angular"],
+        "angularAMD": ["angular"],
+        "ngFileUpload-shim": ["angular"],
+        "ngFileUpload": ["angular"],
+        "ngload": ["angularAMD"]
+    },
+    deps:['app'],
+    urlArgs: "bust=" +  (new Date()).getTime()
+});
