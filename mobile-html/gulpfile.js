a3743f5b397fe531f54db0a6503a2d8105550e6a
spion@qq.com <spion@qq.com>
2017-02-07 17:05:21 +0800

    me

--- mobile-html/gulpfile.js
+++ mobile-html/gulpfile.js
@@ -12,13 +12,22 @@ var gulp = require('gulp') ,
 
 gulp.task('jsmin', function () {
     gulp.src([
+            'js/*.js',
+            'js/**/*.js',
+            '!js/plug/{zepto.min,vue.min,vue-router.min}.js'
+        ]) //多个文件以数组形式传入
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+gulp.task('concat', function () {
+    gulp.src([
             'js/plug/zepto.min.js',
             'js/plug/vue.min.js',
             'js/plug/vue-router.min.js'
         ]) //多个文件以数组形式传入
         .pipe(uglify())
         .pipe(concat('global.js'))
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest('dist/plug'));
 });
 gulp.task('testHtmlmin', function () {
     var options = {
@@ -35,12 +44,12 @@ gulp.task('testHtmlmin', function () {
         .pipe(htmlmin(options))
         .pipe(gulp.dest('dist'));
 });
-gulp.task('build',['jsmin','testHtmlmin'])
+gulp.task('build',['jsmin','concat','testHtmlmin'])
 
 gulp.task('default', function () {  
     //监听js变化  
-    gulp.watch(["js/*.js",'js/**/*.js'], function () {       //当js文件变化后，自动检验 压缩  
+    gulp.watch(["./js/*.js",'./js/**/*.js'], function () {       //当js文件变化后，自动检验 压缩  
         //gulp.run('lint', 'scripts');  
-        gulp.run('lint', 'rjs');  
+        gulp.run('build');  
     });
 }); 
