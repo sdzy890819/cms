031ce0c5bf370ff72faddc923e119331bdd3f0d6
spion@qq.com <spion@qq.com>
2017-02-09 17:48:46 +0800

    me

--- mobile-html/js/config.js
+++ mobile-html/js/config.js
@@ -2,35 +2,13 @@ require.config({
     baseUrl: 'js/',
     paths: {
         'zepto': 'plug/zepto.min',
+        'vue' : 'vue.min',
+        'vue-router' : 'vue-router.min',
         'wangEditor' : 'plug/wangEditor/dist/js/wangEditor',
-
-
-        'angular': 'plug/angular.min',
-        
     },
     shim: {
-        'jquery': {exports: '$'},
-        //'jquery-migrate': ['jquery'],
-        'jcrop': ['jquery'],
-        "uploadify": ["jquery"],
-
-
-        'angular': {exports: 'angular'},
-        'angular-ui-router': {
-            deps: ['angular'] , 
-            exports: 'angular-ui-router'
-        },
-        'angular-css': {
-            deps: ['angular'] , 
-            exports: 'angular-css'
-        },
-        'ui-bootstrap': ["angular"],
-        "angularAMD": ["angular"],
-        "ngFileUpload-shim": ["angular"],
-        "ngFileUpload": ["angular"],
-        "angularFileUpload": ["angular"],
-        "ngload": ["angularAMD"]
+        'zepto': {exports: '$'},
     },
-    deps:['app'],
+    deps:['main'],
     urlArgs: "bust=" +  (new Date()).getTime()
 });
--- mobile-html/js/main.js
+++ mobile-html/js/main.js
@@ -1,1 +1,14 @@
-alert(3)
+import '../css/main.scss'
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+debugger;
+const router = new VueRouter({
+  routes : routes
+})
+const app = new Vue({
+  router : router
+}).$mount('#app');
