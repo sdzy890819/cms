9b7c95001c9f1053095fd579ed510ec38e2ade18
spion@qq.com <spion@qq.com>
2017-02-10 18:38:54 +0800

    me

---
+++ mobile-html/js/login/index.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+asdflksadfsdf
+</div>
+</template>
+<script>
+	export default {
+		name : 'login',
+		props : {
+
+		},
+		data (){
+			return {
+				
+			}
+		},
+		ready(){
+		}
+	}
+</script>
+<style scoped>
+
+</style>
--- mobile-html/js/main.js
+++ mobile-html/js/main.js
@@ -1,16 +1,25 @@
-import '../css/main.scss'
-Vue.use(VueRouter);
+import '../css/main.scss';
+import login from './login/index.vue';
+/*Vue.use(VueRouter);
 
 const Foo = { template: '<div>foo</div>' }
 const Bar = { template: '<div>bar</div>' }
+
 const routes = [
-  { path: '/foo', component: Foo },
+  { path: '/foo', component: login },
   { path: '/bar', component: Bar }
-]
-debugger;
+];
+
 const router = new VueRouter({
   routes : routes
 })
+
+
 const app = new Vue({
   router : router
-}).$mount('#app');
+}).$mount('#app');*/
+
+new Vue({
+	el : '#app',
+	components : { login }
+});
