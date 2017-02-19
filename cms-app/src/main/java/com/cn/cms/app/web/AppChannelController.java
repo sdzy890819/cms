ac88f03f74eca9edf2f373270e82a97942790de3
张阳 <zhangyang04@renrendai.com>
2017-02-19 19:37:30 +0800

    添加频道

---
+++ cms-app/src/main/java/com/cn/cms/app/web/AppChannelController.java
@@ -0,0 +1,52 @@
+package com.cn.cms.app.web;
+
+import com.cn.cms.biz.ChannelBiz;
+import com.cn.cms.po.Channel;
+import com.cn.cms.web.ann.CheckAppAuth;
+import com.cn.cms.web.ann.CheckAppToken;
+import com.cn.cms.web.result.ApiResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * Created by zhangyang on 17/2/19.
+ */
+@Controller
+@RequestMapping(value="/app/channel/",produces = "application/json; charset=UTF-8")
+@ResponseBody
+public class AppChannelController extends AppBaseController {
+
+    @Resource
+    private ChannelBiz channelBiz;
+
+    /**
+     * 获取频道分类列表
+     * @return
+     */
+    @CheckAppToken
+    @CheckAppAuth( name = "appchannel:read")
+    @RequestMapping(value = "/listChannel", method = RequestMethod.GET)
+    public String listChannel(){
+        List<Channel> list = channelBiz.listChannel();
+        return ApiResponse.returnSuccess(list);
+    }
+
+    /**
+     * 获取当前用户的频道分类列表
+     * @return
+     */
+    @CheckAppToken
+    @RequestMapping(value = "/currentChannelList", method = RequestMethod.GET)
+    public String currentChannelList(HttpServletRequest request, @RequestParam(value = "categoryId", required = false) Long categoryId){
+        String userID = getCurrentUserId(request);
+        List<Channel> list = channelBiz.listChannelByUserId(userID, categoryId);
+        return ApiResponse.returnSuccess(list);
+    }
+}
---
+++ cms-app/src/main/java/com/cn/cms/app/web/AppNewsControllerController.java
@@ -0,0 +1,40 @@
+package com.cn.cms.app.web;
+
+import com.cn.cms.biz.NewsBiz;
+import com.cn.cms.po.NewsColumn;
+import com.cn.cms.web.ann.CheckAppAuth;
+import com.cn.cms.web.ann.CheckAppToken;
+import com.cn.cms.web.result.ApiResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by zhangyang on 17/2/19.
+ */
+@Controller
+@RequestMapping(value="/app/newscolumn/",produces = "application/json; charset=UTF-8")
+@ResponseBody
+public class AppNewsControllerController extends AppBaseController {
+
+    @Resource
+    private NewsBiz newsBiz;
+
+    /**
+     * 栏目列表
+     * @param channelId
+     * @return
+     */
+    @CheckAppToken
+    @CheckAppAuth( name = "appnewscolumn:read" )
+    @RequestMapping(value = "/newscolumnlist",method = RequestMethod.GET)
+    public String list(@RequestParam(value = "channelId") Long channelId){
+        List<NewsColumn> list = newsBiz.listNewsColumn(channelId);
+        return ApiResponse.returnSuccess(list);
+    }
+}
