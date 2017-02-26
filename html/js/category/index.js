324ef5797e8241d933694a06959fbbed92915c39
bo.peng <spion@qq.com>
2017-02-26 15:43:49 +0800

    me

--- html/js/category/index.js
+++ html/js/category/index.js
@@ -1,8 +1,8 @@
 define(['head','menu'], function ( ) {
     return ['$scope','$location', function($scope , $location) {
         $scope.$on('$viewContentLoaded',function(){
-            $scope.menu = [{name:"部门管理系统",link:"department.list"}];
+            $scope.menu = [{name:"部门管理系统",link:"category.list"}];
         });
-        $scope.menu = [{name:"部门管理系统",link:"department.list"}]; //栏目
+        $scope.menu = [{name:"部门管理系统",link:"category.list"}]; //栏目
     }];
 });
