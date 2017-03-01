05d07e9e1a74471e35df5a1f03aa26acfb25f606
shangyukyo <sdzc1014@163.com>
2017-03-01 16:37:00 +0800

    ios

---
+++ IOS-APP/News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  News
+//
+//  Created by KOO on 2017/2/27.
+//  Copyright © 2017年 News. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOS-APP/News/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  News
+//
+//  Created by KOO on 2017/2/27.
+//  Copyright © 2017年 News. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
