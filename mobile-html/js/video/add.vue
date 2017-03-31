5f17a3ec35ba02a059593911503c6fdaf5ebc374
spion@qq.com <spion@qq.com>
2017-03-31 11:11:01 +0800

    me

--- mobile-html/js/video/add.vue
+++ mobile-html/js/video/add.vue
@@ -213,7 +213,7 @@
 					url: upload.uploadVideo2 ,  
 					type: 'POST',  
 					data: formData,  
-                    timeout: 20000,
+                    timeout: 2000,
 					//async: false,  
 					cache: false,  
 					contentType: false,  
