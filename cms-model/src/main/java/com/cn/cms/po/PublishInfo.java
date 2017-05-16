2d20767ebc5e1160636653ca1ec05578dbfb12bf
张阳 <zhangyang04@renrendai.com>
2017-05-16 15:29:52 +0800

    添加状态

--- cms-model/src/main/java/com/cn/cms/po/PublishInfo.java
+++ cms-model/src/main/java/com/cn/cms/po/PublishInfo.java
@@ -1,5 +1,8 @@
 package com.cn.cms.po;
 
+import com.cn.cms.enums.PublishStatusEnum;
+import com.cn.cms.enums.TemplateTypeEnum;
+import com.cn.cms.enums.TriggerTypeEnum;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -44,4 +47,32 @@ public class PublishInfo extends Base{
      * 错误信息
      */
     private String errorMessage;
+
+
+    private String triggerTypeStr;
+
+    private String templateTypeStr;
+
+    private String statusStr;
+
+    public String getTriggerTypeStr(){
+        if(triggerType!=null){
+            return TriggerTypeEnum.get(triggerType).getName();
+        }
+        return triggerTypeStr;
+    }
+
+    public String getTemplateTypeStr(){
+        if(templateType!=null){
+            return TemplateTypeEnum.get(templateType).getName();
+        }
+        return templateTypeStr;
+    }
+
+    public String getStatusStr(){
+        if(status!=null){
+            return PublishStatusEnum.get(status).getName();
+        }
+        return statusStr;
+    }
 }
