ac464f4d8309360b506e26929e6c5677bb005d00
spion@qq.com <spion@qq.com>
2017-05-24 10:32:56 +0800

    me

--- html/js/publishInfo/index.js
+++
@@ -1,8 +0,0 @@
-define(['head','menu'], function ( ) {
-    return ['$scope','$location', function($scope , $location) {
-        $scope.$on('$viewContentLoaded',function(){
-            $scope.menu = [{name:"新闻管理",link:"newscolumn.list"}];
-        });
-        $scope.menu = [{name:"新闻管理",link:"newscolumn.list"}]; //栏目
-    }];
-});
--- html/js/publishinfo/index.js
+++
@@ -1,8 +0,0 @@
-define(['head','menu'], function ( ) {
-    return ['$scope','$location', function($scope , $location) {
-        $scope.$on('$viewContentLoaded',function(){
-            $scope.menu = [{name:"发布日志",link:"publishinfo.list"}];
-        });
-        $scope.menu = [{name:"发布日志",link:"publishinfo.list"}]; //栏目
-    }];
-});
