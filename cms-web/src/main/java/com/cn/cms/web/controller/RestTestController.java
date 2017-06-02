f6fc0140d0355e3a657fd9157a3d61dc838e6024
张阳 <zhangyang04@renrendai.com>
2017-06-02 14:29:00 +0800

    reBuildRedis

--- cms-web/src/main/java/com/cn/cms/web/controller/RestTestController.java
+++ cms-web/src/main/java/com/cn/cms/web/controller/RestTestController.java
@@ -1,11 +1,21 @@
 package com.cn.cms.web.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.cn.cms.biz.ChannelBiz;
 import com.cn.cms.biz.NewsBiz;
+import com.cn.cms.biz.UserBiz;
+import com.cn.cms.contants.RedisKeyContants;
 import com.cn.cms.enums.CommonMessageSourceEnum;
 import com.cn.cms.enums.MQQueueKeyEnum;
 import com.cn.cms.message.BuildSendMessage;
 import com.cn.cms.middleware.ESearchClient;
+import com.cn.cms.middleware.JedisClient;
+import com.cn.cms.po.Channel;
 import com.cn.cms.po.News;
+import com.cn.cms.po.NewsColumn;
+import com.cn.cms.po.User;
+import com.cn.cms.service.NewsService;
+import com.cn.cms.service.UserService;
 import com.cn.cms.utils.Page;
 import com.cn.cms.utils.StringUtils;
 import com.cn.cms.web.result.ApiResponse;
@@ -33,7 +43,22 @@ public class RestTestController extends BaseController{
     private NewsBiz newsBiz;
 
     @Resource
+    private NewsService newsService;
+
+    @Resource
     private ESearchClient eSearchClient;
+
+    @Resource
+    private ChannelBiz channelBiz;
+
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private UserBiz userBiz;
+
+    @Resource
+    private JedisClient jedisClient;
 
     @Resource(name="threadTaskExecutor")
     private ThreadPoolTaskExecutor threadPoolTaskExecutor;
@@ -81,6 +106,51 @@ public class RestTestController extends BaseController{
 
         return ApiResponse.returnSuccess();
     }
+
+    @RequestMapping(value = "/reBuildRedis", method = RequestMethod.POST)
+    public String reBuildRedis(@RequestParam(value = "key", required = false) String key){
+        if(StringUtils.isNotBlank(key) && "CMSP5WC".equals(key)){
+            List<Channel> channels = channelBiz.listChannel();
+            if(StringUtils.isNotEmpty(channels)) {
+                for (int i = 0; i < channels.size(); i++) {
+                    if (channels.get(i) != null) {
+                        jedisClient.set(RedisKeyContants.getRedisChannelDetailKey(channels.get(i).getId()), JSONObject.toJSONString(channels.get(i)));
+                    }
+                }
+            }
+
+            Integer countNewsColumn = newsService.queryListCount(null);
+            int pageSize = 5000;
+            if(countNewsColumn!=null && countNewsColumn > 0){
+                pageSize = countNewsColumn;
+            }
+            Page page = new Page(1, pageSize);
+            List<NewsColumn> newsColumns = newsService.queryListForPage(null, page);
+            if(StringUtils.isNotEmpty(newsColumns)){
+                for(int i=0;i<newsColumns.size();i++){
+                    if(newsColumns.get(i)!=null){
+                        this.jedisClient.set(RedisKeyContants.getRedisNewcolumnId(newsColumns.get(i).getId()), JSONObject.toJSONString(newsColumns.get(i)));
+                    }
+                }
+            }
+            Integer count = userService.queryUserCount();
+            pageSize = 10000;
+            if(count!=null && count > 0){
+                pageSize = count;
+            }
+            Page page2 = new Page(1, pageSize);
+            List<User> users = userService.queryUserList(page2);
+            if(StringUtils.isNotEmpty(users)){
+                for(int i=0;i<users.size();i++){
+                    if(users.get(i)!=null){
+                        userBiz.refreshUserCache(users.get(i));
+                    }
+                }
+            }
+        }
+        return ApiResponse.returnSuccess();
+    }
+
 
 
 }
